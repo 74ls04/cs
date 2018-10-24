11baf0aca48e89e73d1d9715c6ab8ed8205ef275
Paul <1887003+74ls04@users.noreply.github.com>
2018-10-24 13:27:02 -0400

    New graph

--- constrained_mst/prim.py
+++ constrained_mst/prim.py
@@ -1,6 +1,10 @@
+# Modification of Prim's MST algorithm to include degree constraints 
+# Minimum Spanning Tree using Boruvka's Algorithm
+# CSCI-7432 - Algorithms and Data Structures
+# Original code for Prim's algorithm from https://coderbyte.com/algorithm/find-minimum-spanning-tree-using-prims-algorithm
+# Paul Bupe
+
 # create adjacency matrix for use in prims algorithm
-# note: we could improve the running time of prims algorithm by
-# implementing a priority queue data structure instead of a matrix
 def createAdjMatrix(V, G):
     adjMatrix = []
 
@@ -14,16 +18,19 @@ def createAdjMatrix(V, G):
     for i in range(0, len(G)):
         adjMatrix[G[i][0]][G[i][1]] = G[i][2]
         adjMatrix[G[i][1]][G[i][0]] = G[i][2]
-
+    # print DataFrame(adjMatrix)
     return adjMatrix
 
 
-def prims(V, G):
+def prims(V, G, C):
     # create adj matrix from graph
     adjMatrix = createAdjMatrix(V, G)
 
     # arbitrarily choose initial vertex from graph
     vertex = 0
+
+    # Keep track of the degree of each vertex
+    degreeList = [0] * V
 
     # initialize empty edges array and empty MST
     MST = []
@@ -42,23 +49,34 @@ def prims(V, G):
         for r in range(0, V):
             if adjMatrix[vertex][r] != 0:
                 edges.append([vertex, r, adjMatrix[vertex][r]])
-
+        
         # find edge with the smallest weight to a vertex
         # that has not yet been visited
+        # Ignore any edge that connects to a "full" vertex
         for e in range(0, len(edges)):
+            if degreeList[edges[e][1]] >= C or degreeList[edges[e][0]] >= C:
+                continue
+
             if edges[e][2] < minEdge[2] and edges[e][1] not in visited:
                 minEdge = edges[e]
+        
+        # Algorithm has failed
+        if minEdge[0] is None:
+            return [0]
+
+        degreeList[minEdge[0]] += 1
+        degreeList[minEdge[1]] += 1
 
         # remove min weight edge from list of edges
         edges.remove(minEdge)
-
+        
         # push min edge to MST
-        MST.append(minEdge)
+        MST.append(minEdge[2])
 
         # start at new vertex and reset min edge
         vertex = minEdge[1]
         minEdge = [None, None, float('inf')]
-
+    # print degreeList
     return MST
 
 
@@ -75,8 +93,8 @@ graph = [
     [b, d, 16],
     [c, d, 8],
     [c, i, 18],
-    [d, i, 4],
-    [d, e, 11],
+    [d, i, 11],
+    [d, e, 4],
     [e, f, 1],
     [e, g, 6],
     [e, h, 5],
@@ -89,4 +107,6 @@ graph = [
 ]
 
 # pass the # of vertices and the graph to run prims algorithm
-print(prims(10, graph))
+mst = prims(10, graph, 3)
+print "Spanning Tree: ", mst
+print "Total cost: ", sum(mst)
